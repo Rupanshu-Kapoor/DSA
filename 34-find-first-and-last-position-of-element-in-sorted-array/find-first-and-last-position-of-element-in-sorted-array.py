5319d96d563fe00e1f7cf1f5f16e4f1525df8763
Rupanshu Kapoor <rupanshukapoor@outlook.com>
2024-02-20 20:06:01 +0530

    Time: 73 ms (78.73%) | Memory: 17.9 MB (71.66%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,13 +1,40 @@
 class Solution:
+    def findFirst(self, nums, target):
+        pass
+        first = -1
+        left, right = 0, len(nums)-1
+
+        while left <= right:
+            mid = (left + right )//2
+
+            if nums[mid] >= target:
+                first = mid
+                right = mid-1
+            else:
+                left = mid + 1
+        if nums[first] != target:
+            first = -1
+        return first
+
+    def findLast(self, nums, target):
+        last = -1
+        left, right = 0, len(nums)-1
+
+        while left <= right:
+            mid = (left + right)//2
+            if nums[mid] <= target:
+                last = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+        if nums[last] != target:
+            last = -1
+        return last
+
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        first = last = -1
-        found = False
+        if len(nums) == 0:
+            return [-1,-1]
+        first = self.findFirst(nums, target)
+        last = self.findLast(nums, target)
 
-        for i in range(len(nums)):
-            if nums[i] == target and not found:
-                first = last = i
-                found = True
-            elif nums[i] == target:
-                last = i
-
-        return [first, last]
+        return [first, last] 
