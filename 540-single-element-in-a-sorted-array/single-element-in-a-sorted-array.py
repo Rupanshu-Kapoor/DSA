6e46ab271753f890414f23f7cfd0c185924bba2f
Rupanshu Kapoor <rupanshukapoor@outlook.com>
2024-03-11 11:33:58 +0530

    Time: 154 ms (5.93%) | Memory: 28.6 MB (6.61%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,16 +1,24 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         # brute force 1
-        count = 0
-        ele = -1
+        # count = 0
+        # ele = -1
 
-        for i in range(len(nums)):
-            if nums[i] == ele:
-                count += 1
-            else:
-                if count == 1:
-                    return ele
-                else:
-                    count = 1
-                    ele = nums[i]
-        return ele
+        # for i in range(len(nums)):
+        #     if nums[i] == ele:
+        #         count += 1
+        #     else:
+        #         if count == 1:
+        #             return ele
+        #         else:
+        #             count = 1
+        #             ele = nums[i]
+        # return ele
+
+        # brute force 2: hash map
+        h_map = {}
+        for i in nums:
+            h_map[i] =h_map.get(i,0)+1
+        for i in h_map:
+            if h_map[i] == 1:
+                return i
